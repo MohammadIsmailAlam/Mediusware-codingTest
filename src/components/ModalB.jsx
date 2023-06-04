06ab6a073918d69fa3a42e63a48f8d5b018fdfdb
mohammadIsamil <ismailalam901@gmail.com>
2023-06-05 00:57:18 +0600

    problem-2 retrying

--- src/components/ModalB.jsx
+++ src/components/ModalB.jsx
@@ -1,81 +1,87 @@
-import { useEffect, useState } from "react";
+import React, { useEffect, useState } from "react";
+import { Button } from "react-bootstrap";
 import Modal from "react-bootstrap/Modal";
 import { useNavigate } from "react-router-dom";
 
 export default function ModalB() {
   const [show, setShow] = useState(true);
   const [contacts, setContacts] = useState([]);
-  const [onlyEvenChecked, setOnlyEvenChecked] = useState(false);
-  const [searchInput, setSearchInput] = useState("");
+  const [showContacts, setShowContacts] = useState(false);
   const [filteredContacts, setFilteredContacts] = useState([]);
-  
-const navigate = useNavigate();
+  const [onlyEvenChecked, setOnlyEvenChecked] = useState(false);
+
+  const navigate = useNavigate();
+
   const handleClose = () => {
     setShow(false);
     navigate(-1);
-  }
+  };
+
+  const handleShowAllContacts = () => {
+    setShowContacts(true);
+    setFilteredContacts(contacts);
+  };
+
+  const handleShowUSContacts = () => {
+    setShowContacts(true);
+    setFilteredContacts(
+      contacts.filter((data) => data.country.name === "United States")
+    );
+  };
+
+  const handleOnlyEvenChange = () => {
+    setOnlyEvenChecked(!onlyEvenChecked);
+    if (!onlyEvenChecked) {
+      setFilteredContacts(contacts.filter((data) => data.id % 2 === 0));
+    } else {
+      if (showContacts) {
+        handleShowAllContacts();
+      }
+    }
+  };
+
   useEffect(() => {
     // Fetch all contacts from the API
     fetch("https://contact.mediusware.com/api/contacts")
       .then((response) => response.json())
       .then((data) => {
-        setContacts( data.results);
+        setContacts(data.results);
       })
       .catch((error) => {
         console.error("Error fetching contacts:", error);
       });
   }, []);
 
-  useEffect(() => {
-    // Filter contacts based on the checkbox and search input
-    let filtered = contacts;
-    if (onlyEvenChecked) {
-      filtered = filtered.filter((contact) => contact.id % 2 === 0);
-    }
-    if (searchInput) {
-      filtered = filtered.filter((contact) =>
-        contact.name.toLowerCase().includes(searchInput.toLowerCase())
-      );
-    }
-    setFilteredContacts(filtered);
-  }, [contacts, onlyEvenChecked, searchInput]);
-
-  const fetchFilteredContacts = () => {
-    const url = `https://contact.mediusware.com/api/contacts?country=${
-      modalAOpen ? "all" : "US"
-    }&name=${searchInput}`;
-    fetch(url)
-      .then((response) => response.json())
-      .then((data) => {
-        setContacts(data.contacts);
-      })
-      .catch((error) => {
-        console.error("Error fetching filtered contacts:", error);
-      });
-  };
-
   return (
     <>
-      <Modal
-        show={show}
-        onHide={handleClose}
-        backdrop="static"
-        keyboard={false}
-      >
-        <Modal.Header closeButton>
+      <Modal show={show} backdrop="static" keyboard={false}>
+        <Modal.Header>
           <Modal.Title>Modal title</Modal.Title>
         </Modal.Header>
         <Modal.Body>
-          {
-            contacts.filter(data=>data.country.name == "United States").map((data, index) => {
-             return <li key={index}>
-              {data.id}
-              {data.phone}
-              {data.country.name}
-             </li>
-            })
-          }
+          <Button onClick={handleShowAllContacts}>Button A</Button>
+          <Button onClick={handleShowUSContacts}>Button B</Button>
+          <Button onClick={handleClose}>Button C</Button>
+          {showContacts && (
+            <ul>
+              {filteredContacts.map((data, index) => (
+                <li key={index}>
+                  {data.id} {data.phone} {data.country.name}
+                </li>
+              ))}
+            </ul>
+          )}
         </Modal.Body>
+        <Modal.Footer>
+          <label>
+            <input
+              type="checkbox"
+              checked={onlyEvenChecked}
+              onChange={handleOnlyEvenChange}
+            />
+            Only even
+          </label>
+        </Modal.Footer>
       </Modal>
     </>
   );
